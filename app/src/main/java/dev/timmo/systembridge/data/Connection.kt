9376f2a7732edcdc97449ecba45e0a089619c478
Aidan Timson <contact@timmo.xyz>
2021-09-12 13:26:11 +0100

    Add wsPort

--- app/src/main/java/dev/timmo/systembridge/data/Connection.kt
+++ app/src/main/java/dev/timmo/systembridge/data/Connection.kt
@@ -9,5 +9,6 @@ data class Connection(
     @PrimaryKey val uid: Int,
     @ColumnInfo(name = "host") val host: String,
     @ColumnInfo(name = "api_port") val apiPort: Int,
+    @ColumnInfo(name = "ws_port") val wsPort: Int,
     @ColumnInfo(name = "api_key") val apiKey: String,
 )
