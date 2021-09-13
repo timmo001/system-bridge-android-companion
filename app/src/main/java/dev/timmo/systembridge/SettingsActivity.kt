75d4a45a65660affa867b4b44cec3f757b0483b8
Aidan Timson <contact@timmo.xyz>
2021-09-13 17:42:38 +0100

    Get data

--- app/src/main/java/dev/timmo/systembridge/SettingsActivity.kt
+++ app/src/main/java/dev/timmo/systembridge/SettingsActivity.kt
@@ -20,31 +20,42 @@ import kotlinx.coroutines.launch
 @SuppressLint("NotifyDataSetChanged")
 class SettingsActivity : AppCompatActivity() {
     private lateinit var connectionData: List<Connection>
+
+    private lateinit var recyclerViewBridges: RecyclerView
     private lateinit var bridgesAdapter: BridgesRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        findViewById<RecyclerView>(R.id.recyclerViewBridges).also { recyclerView: RecyclerView ->
+        recyclerViewBridges = findViewById<RecyclerView>(R.id.recyclerViewBridges).also { recyclerView: RecyclerView ->
             recyclerView.layoutManager = GridLayoutManager(this, 1)
-
-            GlobalScope.launch(Dispatchers.IO) {
-                connectionData =
-                    AppDatabase.getInstance(applicationContext).connectionDao().getAll()
-
-                Log.d("SettingsActivity", connectionData.toString())
-
-                launch(Dispatchers.Main) {
-                    Log.d("SettingsActivity", "Set adapter")
-                    bridgesAdapter = BridgesRecyclerViewAdapter(connectionData)
-                    recyclerView.adapter = bridgesAdapter
-                }
-            }
         }
+
+        getData()
 
         findViewById<Button>(R.id.buttonAddNewBridge).setOnClickListener {
             startActivity(Intent(this, SetupActivity::class.java))
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getData()
+    }
+
+    private fun getData() {
+        GlobalScope.launch(Dispatchers.IO) {
+            connectionData =
+                AppDatabase.getInstance(applicationContext).connectionDao().getAll()
+
+            Log.d("SettingsActivity", connectionData.toString())
+
+            launch(Dispatchers.Main) {
+                Log.d("SettingsActivity", "Set adapter")
+                bridgesAdapter = BridgesRecyclerViewAdapter(connectionData)
+                recyclerViewBridges.adapter = bridgesAdapter
+            }
         }
     }
 
